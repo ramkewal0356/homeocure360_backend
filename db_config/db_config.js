2d9c59334351e52e00ac3fa26efbdf080edfa4c1
ramkewal0356 <ram.kewal0356@gmail.com>
2025-10-25 15:47:06 +0530

    vercel setup1

--- db_config/db_config.js
+++ db_config/db_config.js
@@ -1,16 +1,39 @@
+// const mongoose = require('mongoose');
+// const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/homeo_cure';
+
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(MONGO_URI);
+//         console.log('MongoDB connected');
+//     } catch (err) {
+//         console.error('MongoDB connection error:', err);
+//         process.exit(1); // exit process if connection fails
+//     }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/homeo_cure';
 
+let cached = global.mongoose;
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(MONGO_URI);
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error('MongoDB connection error:', err);
-        process.exit(1); // exit process if connection fails
-    }
-};
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 module.exports = connectDB;
-
